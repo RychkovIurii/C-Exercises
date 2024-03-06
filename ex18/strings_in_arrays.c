d476161619daa153b5de8e848f67cc225e8fe316
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-03-06 21:35:35 +0200

    string/s in array/s

---
+++ ex18/strings_in_arrays.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(void){
+     //! showMemory(cursors=[words[0], words[1], words[2]], start=65520)
+    char a[4];
+    char b[6];
+    char c[9];
+    char * words[3] = {a, b, c};
+    printf("Please enter a word with at  most 3 letters: ");
+    scanf("%s", a);
+    printf("Please enter a word with at  most 5 letters: ");
+    scanf("%s", b);
+    printf("Please enter a word with at  most 8 letters: ");
+    scanf("%s", c);
+    printf("You entered: \n");
+    printf("%s %s %s.\n", a, b, c);
+    printf("%s %s %s.\n", words[0], words[1], words[2]);
+    return 0;
+}
+
+// main()
+// char a[4] = {'T', 'h', 'e', \0}
+// char b[6] = {'g', 'r', 'e', 'a', 't', \0}
+// char c[9] = {'o', 'u', 't', 'd', 'o', 'o', 'r', 's', \0}
+// char *words[3] = {0xfffc, 0xfff6, 0xffed}
+
+// Terminal
+// Please enter a word with at  most 3 letters: The 
+// Please enter a word with at  most 5 letters: great
+// Please enter a word with at  most 8 letters: outdoors
+// You entered: 
+// The great outdoors.
+// The great outdoors.
+ 
