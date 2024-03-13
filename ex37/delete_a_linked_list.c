17c0a7f86f73fa9a191348768a8b56b5e4dcda27
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-03-13 09:37:29 +0200

    Delete a linked list. Pointers

--- ex37/delete_a_linked_list.c
+++ ex37/delete_a_linked_list.c
@@ -69,3 +69,16 @@ void freePoints(struct point * start) {
         free(start);
     }
 }
+
+// Suppose that we have defined a student record as follows:
+// struct student {
+// 	char name[50];
+// 	int age;
+// 	struct student *next;
+// };
+// Suppose furthermore you have created a student database in the form of a linked list and suppose that the pointer start (of type struct student *) points to the beginning of this list. How would you print the name and age of the first student in the list?
+// printf("%s is %d years old.", start->name, start->age);
+
+
+// As in the previous question, you have defined a student record as follows: struct student { char name[50]; int age; struct student *next; }; and have created a student database in the form of a linked list with the pointer start (of type struct student *) pointing to the beginning of this list. How would you print the name and age of the second student in the list?
+// printf("%s is %d years old.", start->next->name, start->next->age);
