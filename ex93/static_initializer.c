536d2ac6530f31d497938caa9511497a9a70425d
Rychkov Iurii <irychkov@student.hive.fi>
2024-11-08 15:36:14 +0200

    draft

---
+++ ex93/static_initializer.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   static_initializer.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 12:33:08 by irychkov          #+#    #+#             */
+/*   Updated: 2024/11/08 12:36:12 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+
+#define THREAD_NUM 2
+
+pthread_mutex_t	mutex_fuel;
+
+void	*routine(void *arg)
+{
+
+}
+
+int	main(int ac, char *av[])
+{
+	int			i;
+	pthread_t	th[THREAD_NUM];
+
+	i = 0;
+	pthread_mutex_init(&mutex_fuel, NULL);
+	while (i < THREAD_NUM)
+	{
+		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
+		{
+		perror("pthread_create failed");
+		}
+		i++;
+	}
+	i = 0;
+	while (i < THREAD_NUM)
+	{
+		if (pthread_join(th[i], NULL) != 0)
+		{
+			perror("pthread_join failed");
+		}
+		i++;
+	}
+	pthread_mutex_destroy(&mutex_fuel);
+	return (0);
+}
