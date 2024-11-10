a14f129837ee07cc503504fcdecd56d521fa88f7
Iurii Rychkov <rychkoviurii@hotmail.com>
2024-11-10 19:18:59 +0200

    static_initializer

--- ex93/static_initializer.c
+++ ex93/static_initializer.c
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 12:33:08 by irychkov          #+#    #+#             */
-/*   Updated: 2024/11/08 12:36:12 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/11/10 19:17:32 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 
 #define THREAD_NUM 2
 
-pthread_mutex_t	mutex_fuel;
+pthread_mutex_t	mutex_fuel = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t	cond_fuel = PTHREAD_COND_INITIALIZER;
 
 void	*routine(void *arg)
 {
@@ -30,7 +31,7 @@ int	main(int ac, char *av[])
 	pthread_t	th[THREAD_NUM];
 
 	i = 0;
-	pthread_mutex_init(&mutex_fuel, NULL);
+	/* pthread_mutex_init(&mutex_fuel, NULL); */
 	while (i < THREAD_NUM)
 	{
 		if (pthread_create(&th[i], NULL, &routine, NULL) != 0)
@@ -48,6 +49,6 @@ int	main(int ac, char *av[])
 		}
 		i++;
 	}
-	pthread_mutex_destroy(&mutex_fuel);
+	/* pthread_mutex_destroy(&mutex_fuel); */
 	return (0);
 }
